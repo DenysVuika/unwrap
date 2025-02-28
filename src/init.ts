a060b6cfa2807e42a9693eb1594819c0246aa1b5
Denys Vuika <denys.vuika@gmail.com>
2025-02-28 12:08:03 -0500

    init command

--- src/init.ts
+++ src/init.ts
@@ -1,10 +1,10 @@
+import { version } from '../package.json';
 const APP_DATA_DIR = '.unwrap';
 
 import fs from 'fs';
 
 export function init() {
   const currentDirectory = process.cwd();
-  console.log(currentDirectory);
 
   if (fs.existsSync(`${currentDirectory}/${APP_DATA_DIR}`)) {
     console.log('Project already initialised');
@@ -12,19 +12,37 @@ export function init() {
   }
 
   fs.mkdirSync(`${currentDirectory}/${APP_DATA_DIR}`);
-  console.log('Project initialised');
+  updateGitignore(currentDirectory);
 
-  // if current directory contains .gitignore, add .unwrap to .gitignore (only if it's not already there)
-  if (fs.existsSync(`${currentDirectory}/.gitignore`)) {
-    const gitignore = fs.readFileSync(`${currentDirectory}/.gitignore`, 'utf8');
-    if (gitignore.includes(APP_DATA_DIR)) {
-      console.log('.unwrap already in .gitignore');
-      return;
+  // create the .unwrap.json config file in the .unwrap directory
+  const config = generateConfig();
+  fs.writeFileSync(
+    `${currentDirectory}/${APP_DATA_DIR}/.unwrap.json`,
+    JSON.stringify(config, null, 2)
+  );
+
+  console.log('Project initialised');
+}
+
+function updateGitignore(currentDirectory: string) {
+  const filePath = `${currentDirectory}/.gitignore`;
+
+  if (fs.existsSync(filePath)) {
+    const gitignore = fs.readFileSync(filePath, 'utf8');
+
+    if (!gitignore.includes(APP_DATA_DIR)) {
+      fs.appendFileSync(filePath, `\n${APP_DATA_DIR}`);
+      console.log('.unwrap added to .gitignore');
     }
-    fs.appendFileSync(`${currentDirectory}/.gitignore`, `\n${APP_DATA_DIR}`);
-    console.log('.unwrap added to .gitignore');
   } else {
-    // create .gitignore with .unwrap
-    fs.writeFileSync(`${currentDirectory}/.gitignore`, `${APP_DATA_DIR}\n`);
+    fs.writeFileSync(filePath, `${APP_DATA_DIR}`);
   }
+}
+
+function generateConfig() {
+  const config = {
+    version,
+  };
+
+  return config;
 }
